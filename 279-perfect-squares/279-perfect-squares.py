2ba7edf5e5f229dcfa6e219b4b832826153323ba
b0ho <ybg4828@gmail.com>
2022-03-09 00:25:33 +0900

    Time: 138 ms (92.46%), Space: 13.9 MB (94.99%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,12 +1,17 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         def is_square(num):
+            # num의 제곱수 여부를 리턴
             return (int(num**0.5))**2 == num
         
         if is_square(n):
+            # 자기 자신이 완전 제곱수인 경우는 1이 최소
             return 1
         
         for i in range(int(n**0.5) + 1):
+            # (자기 자신 - 제곱수*제곱수)가 제곱수인 경우
+            # 이 경우 자기 자신 + 제곱수 = 2가 최소
+            # 필요한 정답은 정수를 이루는 최소 제곱수의 개수
             if is_square(n - i**2):
                 return 2
             
@@ -15,6 +20,10 @@ class Solution:
                 if (n - i**2 - j**2) < 0:
                     break
                 elif is_square(n - i**2 - j**2):
-                    return 3	
+                    return 3
                 
+        # [그랑주 네 제곱수 정리]
+        # 임의의 양수 n은 최대 4개의 제곱수만을 가질 수 있음
+        # 따라서 1,2,3 이 아닌 경우는 4일 수 밖에 없음
         return 4
+    
